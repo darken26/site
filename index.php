9ae700befc259361f1755b26a82be0ff1cac8f88
Eudis Duran <eudisd@gmail.com>
2011-07-30 13:06:08 -0700

    Adding data folder and a few images.  Also started work on the main page

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang='en'>
+    <head>  
+        <meta http-equiv='Content-Type' content='text/html;charset=UTF8' />
+        <link rel='stylesheet' href='css/base.css' type='text/css' />
+        <script type='text/javascript' src='http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js'></script>
+        <script type='text/javascript' src='js/main.js'></script>
+    </head>
+    <body>  
+        <div id='container'>
+            <div id='header'>
+                <div id='name'> Eudis Duran </div>
+                <div id='icons'> </div>
+            </div>
+            <div id='content'>
+                Main Content
+            </div>
+            <div id='footer'>
+                Footer
+            </div>
+        </div>
+    </body>
+</html>
