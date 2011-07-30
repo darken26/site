8618d805b120cb7df6e3ce3d0936b27e9126bf29
Eudis Duran <eudisd@gmail.com>
2011-07-30 13:42:02 -0700

    Added gradient filter for backgroud and worked a bit more on the css side of things

--- index.php
+++ index.php
@@ -1,16 +1,28 @@
 <!DOCTYPE html>
 <html lang='en'>
     <head>  
-        <meta http-equiv='Content-Type' content='text/html;charset=UTF8' />
+        <title> Eudis Duran </title>
+        <meta http-equiv='Content-Type' content='text/html; charset=UTF-8' />
         <link rel='stylesheet' href='css/base.css' type='text/css' />
+        <link rel='shortcut icon' href='data/favicon.png' type='image/png' />
         <script type='text/javascript' src='http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js'></script>
         <script type='text/javascript' src='js/main.js'></script>
     </head>
     <body>  
         <div id='container'>
             <div id='header'>
-                <div id='name'> Eudis Duran </div>
-                <div id='icons'> </div>
+                <div id='name'> 
+                    Eudis Duran
+                </div>
+                <div id='subtitle'>
+                    - Programmer -
+                </div>
+                <div id='icons'> 
+                    <a href='#'><img class='icon' src='data/twitter_icon.png' alt='My Twitter Page' /></a>
+                    <a href='#'><img class='icon' src='data/facebook_icon.png' alt='My FB Page' /></a>
+                    <a href='#'><img class='icon' src='data/linkedin_icon.png' alt='My Linkedin Page' /></a>
+                    <a href='#'><img class='icon' src='data/github_icon.png' alt='My Github Page' /></a>
+                </div>
             </div>
             <div id='content'>
                 Main Content
