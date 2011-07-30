7183ddae66ddea4d1b45f6dfb2266e0c026f6a0a
Eudis Duran <eudisd@gmail.com>
2011-07-30 16:24:47 -0700

    Added graphic for the tooltips as well as the jquery tooltip library

--- js/main.js
+++ js/main.js
@@ -1,13 +1,12 @@
 
 $(document).ready(function() {
     
-    //$.bindIconToolTips();
+    $.bindIconToolTips();
 
     $.hoverEffect("#twitter");
     $.hoverEffect("#fb");
     $.hoverEffect("#linkedin");
     $.hoverEffect("#github");
-
 
     // Use Sam's Color.js to interpolate my name :)
     Clock(document.getElementById('name'), { type: 'm', startColor: [230, 230, 250], endColor: [50, 50, 70], tickInterval: 50}); 
