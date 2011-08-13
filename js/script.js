49fadb125aac8f473fec25d6b1dadd95891858d0
Eudis Duran <eudisd@gmail.com>
2011-08-13 11:28:54 -0700

    Added mootools to the project (virtue of mootools). Added LightFace modal dialog library to the project. Included the google loader for efficient loading. Made mootools and jquery work nicely together (via closures and $.noConflict())

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
 
-$(document).ready(function() {
+jQuery(document).ready(function() {
+   (function($) { 
+      $.bindIconToolTips();
     
-    $.bindIconToolTips();
+      $.hoverEffect("#twitter");
+      $.hoverEffect("#fb");
+      $.hoverEffect("#linkedin");
+      $.hoverEffect("#github");
     
-    $.hoverEffect("#twitter");
-    $.hoverEffect("#fb");
-    $.hoverEffect("#linkedin");
-    $.hoverEffect("#github");
-    
-    // Use Sam's Color.js to interpolate my name :)
-    //Clock(document.getElementById('name'), { type: 'm', startColor: [230, 230, 250], endColor: [50, 50, 70], tickInterval: 50}); 
+      // Use Sam's Color.js to interpolate my name :)
+      //Clock(document.getElementById('name'), { type: 'm', startColor: [230, 230, 250], endColor: [50, 50, 70], tickInterval: 50}); 
+   })(jQuery);
 });
 
