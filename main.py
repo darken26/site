d4b9682e7b6736a1ceb55071735407ae12c44db2
Eudis Duran <eudisd@live.com>
2011-12-22 17:32:10 -0500

    Some ajax progress.  Also added ajax gif

--- main.py
+++ main.py
@@ -25,12 +25,28 @@ class ResumePage(webapp.RequestHandler):
         path = os.path.join(os.path.dirname(__file__), 'resume.html')
 
         self.response.out.write(template.render(path, {}))
+		
+class Page1Page(webapp.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/html'
+        path = os.path.join(os.path.dirname(__file__), 'templates/page1.html')
 
+        self.response.out.write(template.render(path, {}))
+
+class Page2Page(webapp.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/html'
+        path = os.path.join(os.path.dirname(__file__), 'templates/page2.html')
+
+        self.response.out.write(template.render(path, {}))
+
+		
 def main():
     application = webapp.WSGIApplication([('/', MainPage),
         ('/resume', ResumePage),
+		('/page1', Page1Page),
+		('/page2', Page2Page),
         ('/.*', NoPageFound)
-        
         ], 
         debug=False) 
     run_wsgi_app(application)
